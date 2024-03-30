b18af62f080e63e203a4568145e13a2bc3d48593
Pabmoy25 <pabmoy1725@gmail.com>
2024-03-29 23:26:19 -0300

    se crea un archivo estatico en la ruta principal del backend

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import 'dotenv/config';
 import cors from "cors";
 import morgan from "morgan";
+import path from 'path'
+import { fileURLToPath } from "url";
 
 const app = express();
 
@@ -12,7 +14,13 @@ app.listen(app.get('port'), ()=>    {
 })
 
 app.use(cors());
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname,'/public')))
 
 app.get('/',(req, res)=> {
     console.log('prueba de solicitud get');
