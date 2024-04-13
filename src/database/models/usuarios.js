ac69763f8b0fc3eea5cfb8cbfed38db33000c3e7
fatiperea <fatimaperea3pppp0@gmail.com>
2024-04-13 19:18:19 -0300

    agrego archivo .env,schema y validaciones

--- src/database/models/usuarios.js
+++ src/database/models/usuarios.js
@@ -1,1 +1,31 @@
 import mongoose from "mongoose";
+
+const usuarioSchema = new mongoose.Schema({
+    email: {
+      type: String,
+      require: true,
+      validate: {
+          validator: (value)=>{
+              const pattern = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i
+              return pattern.test(value)
+          }
+      }
+    },
+    password: {
+      type: String,
+      require: true
+    },
+    role: {
+      type: String,
+      require: true,
+    },
+    nombreUsuario: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 4,
+      maxLength: 15
+    },
+  });
+  
+   const Usuario = mongoose.model('usuario', usuarioSchema);
