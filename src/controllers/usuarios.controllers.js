da12e7ef78f31d1ee2f11ea16e8715d2ab5012cf
fatiperea <fatimaperea3pppp0@gmail.com>
2024-04-14 03:57:36 -0300

    corrijo app.use para usuario y se logra crear

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -4,6 +4,8 @@ export const crearUsuario = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    console.log("busco ", email);
+
     const usuarioBuscado = await Usuario.findOne({ email });
 
     if (usuarioBuscado) {
@@ -11,18 +13,16 @@ export const crearUsuario = async (req, res) => {
         .status(400)
         .json({ mensaje: "El correo ya se encuentra registrado" });
     }
-
     const nuevoUsuario = new Usuario(req.body);
 
     nuevoUsuario.save();
-
     res.status(201).json({
       mensaje: "Usuario creado correctamente",
       email: nuevoUsuario.email,
       nombre: nuevoUsuario.nombreCompleto,
     });
   } catch (error) {
-    console.error(error);
+    console.log(error);
     res.status(500).json({ mensaje: "Error al intentar crear un usuario" });
   }
 };
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,6 +3,6 @@ import { crearUsuario } from "../controllers/usuarios.controllers.js";
 
 const router = Router();
 
-router.route("/usuarios").post(crearUsuario);
+router.route("/crear").post(crearUsuario);
 
 export default router;
