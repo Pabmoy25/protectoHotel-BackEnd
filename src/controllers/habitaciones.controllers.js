01a86d7b033b9605d61d06182361631ef1b05f25
Antonella Ortega <antonellaportega@gmail.com>
2024-04-20 21:37:09 -0300

    funcion editar habitacion

--- src/controllers/habitaciones.controllers.js
+++ src/controllers/habitaciones.controllers.js
@@ -25,3 +25,20 @@ export const crearHabitaciones = async (req, res) => {
     });
   }
 };
+
+export const editarHabitacion = async (req,res) => {
+  try{
+    const habitacionBuscado = await Habitacion.findById(req.params.id);
+    if(!habitacionBuscado){
+        return res.status(404).json({mensaje:"No se encontro la habitacion con el id especificado"});
+    }
+    await Habitacion.findByIdAndUpdate(req.params.id, req.body)
+    //responder al usuario
+    res.status(200).json({mensaje: "La Habitacion fue editada exitosamente"});
+ }
+ catch(error){
+    console.error(error);
+    res.status(500).json({mensaje:"Ocurrio un error no se pudo editar la habitacion"});
+ }
+};
+
