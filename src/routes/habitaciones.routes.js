7a5c48dbc175faa751f1802646816564faa4730b
Agostina Bellotti <agostinabell@gmail.com>
2024-04-20 22:57:59 -0300

    Se importo la funcion borrar en habitaciones.routes

--- src/routes/habitaciones.routes.js
+++ src/routes/habitaciones.routes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import {
   crearHabitaciones,
   listarHabitaciones,
+  borrarHabitacion,
 
 } from "../controllers/habitaciones.controllers.js";
 import validarJWT from "../helpers/validarJWT.js";
 
 const router = Router();
 
-router.route("/habitaciones").get(listarHabitaciones).post(/* [validarJWT], */crearHabitaciones);// [validarJWT] pide token antes de crear, agregarlo en editar y borrar
+router.route("/habitaciones").get(listarHabitaciones).post(/* [validarJWT], */crearHabitaciones).delete(borrarHabitacion);
 
 export default router;
