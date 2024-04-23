adb6c9e65b5d6a8ffe2009f952d8d9aff8612d70
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-04-23 03:40:36 -0300

    se creo el archivo validacionHabitacion para reorganizar la logica

--- src/routes/habitaciones.routes.js
+++ src/routes/habitaciones.routes.js
@@ -20,11 +20,19 @@ router
         .withMessage("El número de habitacion es un dato obligatorio")
         .isLength({min: 2, max: 5})
         .withMessage('El número de habitacion debe tener entre 2 y 5 caracteres'),
-        check("precio")
+
+      check("precio")
         .notEmpty()
         .withMessage("El precio es un dato obligatorio")
         .isNumeric()
-        .withMessage('El precio debe ser un número'),
+        .withMessage('El precio debe ser un número')
+        .custom((value)=>{
+if(value >50 && value < 10000){
+  return true;
+}else{
+  throw new Error('El precio debe estar entre $10.000 y $100.000')
+}
+        }),
 
 
         check ('tipoDeHabitacion')
