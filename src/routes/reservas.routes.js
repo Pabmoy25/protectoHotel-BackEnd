b3a76106a4268cf65fad761430290afb24fa1f7e
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-04-27 11:48:43 -0300

    agregue las routes para crear y listar reservas

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -1,6 +1,11 @@
 import {Router} from "express";
+import { crearReservas, listarReservas } from "../controllers/reserva.controllers";
 
 const router = Router();
 
+router
+  .route("/reservas")
+  .get(listarReservas)
+  .post(crearReservas);
 
 export default router
