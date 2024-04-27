e6833049045df88b93360192bb0033ae909b79cd
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-04-27 11:50:29 -0300

    agregue la extension .js a la ruta de los import crearReservas y listarReservas

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { crearReservas, listarReservas } from "../controllers/reserva.controllers";
+import { crearReservas, listarReservas } from "../controllers/reserva.controllers.js";
 
 const router = Router();
 
