d18fda2addba9adee3df5b9a9c956a6f4015d5c8
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-05-07 11:05:10 -0300

    se creo la ruta para editar reservas

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -7,6 +7,7 @@ import {
   obtenerUsuarios,
 } from "../controllers/usuarios.controllers.js";
 import validacionUsuario from "../helpers/validacionUsuario.js";
+import { editarReserva } from "../controllers/reserva.controllers.js";
 
 const router = Router();
 
@@ -20,6 +21,7 @@ router
   .route("/:id")
   .get(obtenerUsuarios)
   .put(editarUsuarios)
+  .put(editarReserva)
   
 
 
