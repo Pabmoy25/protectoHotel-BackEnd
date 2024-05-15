ec1da3d357bf74a4415b7d9c964bf42578fbd435
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-05-15 15:27:00 -0300

    se realiza la validacion de habitacion y se prueba su funcionamiento

--- src/helpers/validacionReserva.js
+++ src/helpers/validacionReserva.js
@@ -2,7 +2,15 @@ import { check } from "express-validator";
 import resultadoValidacionReserva from "./resultadoValidacion.js";
 
 const validacionReserva = [
-  
+  check("habitacion")
+    .notEmpty()
+    .withMessage("El número de habitacion es un dato obligatorio")
+    .isLength({ min: 2, max: 5 })
+    .withMessage("El número de habitacion debe tener entre 2 y 5 caracteres"),
+
+ 
+
+    (req, res, next) => resultadoValidacionReserva(req, res, next),
 ];
 
 
