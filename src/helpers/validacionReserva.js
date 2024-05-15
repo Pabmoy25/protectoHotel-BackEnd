6822ef0c0053a4f9de0a83fb526ff320372e0b0d
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-05-15 15:28:23 -0300

    se realiza la validaciones de items correspondientes al formulario de reserva

--- src/helpers/validacionReserva.js
+++ src/helpers/validacionReserva.js
@@ -8,7 +8,42 @@ const validacionReserva = [
     .isLength({ min: 2, max: 5 })
     .withMessage("El número de habitacion debe tener entre 2 y 5 caracteres"),
 
- 
+  check("precio")
+    .notEmpty()
+    .withMessage("El precio es un dato obligatorio")
+    .isNumeric()
+    .withMessage("El precio debe ser un número")
+    .custom((value) => {
+      if (value > 10000 && value < 100000) {
+        return true;
+      } else {
+        throw new Error("El precio debe estar entre $10.000 y $100.000");
+      }
+    }),
+
+  check("tipoDeHabitacion")
+    .notEmpty()
+    .withMessage("El tipo de habitacion es un dato obligatorio")
+    .isIn(["Estándar", "Doble", "Deluxe", "Suite"])
+    .withMessage(
+      'El tipo de habitacion debe ser una de las siguientes opciones "Estándar", "Doble", "Deluxe", "Suite"'
+    ),
+
+  check("nombreCompleto")
+    .notEmpty()
+    .withMessage("El nombre es obligatorio")
+    .isLength({ min: 3, max: 50 })
+    .withMessage("El nombre debe contener entre 4 y 50 caracteres"),
+
+  check("email")
+    .notEmpty()
+    .withMessage("El e-mail es obligatorio")
+    .isLength({ min: 10, max: 40 })
+    .withMessage("El e-mail debe contener entre 15 y 30 caracteres")
+    .matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/),
+
+  
+
 
     (req, res, next) => resultadoValidacionReserva(req, res, next),
 ];
