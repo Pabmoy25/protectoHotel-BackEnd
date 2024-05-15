810935bfd0f774bc763ddae63c0f40bb35f32121
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-05-15 16:39:39 -0300

    agrega a la rutas de solicitud crear y editar de reservas las validaciones de express-validator realizadas

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -1,18 +1,19 @@
 import {Router} from "express";
 import { borrarReserva, crearReservas, editarReserva, listarReservas, obtenerReserva} from "../controllers/reserva.controllers.js";
+import validacionReserva from "../helpers/validacionReserva.js";
 
 const router = Router();
 
 router
   .route("/reservas")
   .get(listarReservas)
-  .post(crearReservas)
+  .post([validacionReserva],crearReservas)
   
 
   router
   .route("/reservas/:id")
   .delete(borrarReserva)
-  .put(editarReserva)
+  .put([validacionReserva], editarReserva)
   .get (obtenerReserva)
 
   
