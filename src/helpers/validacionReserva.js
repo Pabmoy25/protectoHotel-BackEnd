1baa742e6eb2ca0bd9c35791ca2684dc9626a94e
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-05-15 15:26:06 -0300

    se creo el componente ValidacionReserva

---
+++ src/helpers/validacionReserva.js
@@ -0,0 +1,10 @@
+import { check } from "express-validator";
+import resultadoValidacionReserva from "./resultadoValidacion.js";
+
+const validacionReserva = [
+  
+];
+
+
+
+export default validacionReserva;
